ad108389bc478f6a18a698416e98b72da1d73f71
shawnchoi8 <bychoise8@gmail.com>
2025-05-20 20:59:14 +0900

    order read V4 : direct DTO projection in JPA

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.repository.order.query;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderQueryRepository {
+
+    private final EntityManager em;
+
+    public List<OrderQueryDto> findOrderQueryDtos() {
+        List<OrderQueryDto> result = findOrders(); //orders 가져와
+
+        result.forEach(o -> {
+            List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+            o.setOrderItems(orderItems);
+        });
+
+        return result;
+    }
+
+    /**
+     * Reason for not using join fetch:
+     * This query does not fetch entire entity objects,
+     * but rather selects specific fields to map into a DTO.
+     * join fetch cannot be used in DTO projections.
+     */
+
+    //collection 아닌 부분들 join해서 한방쿼리로 보내기
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+    }
+
+    //collection
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) { //
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id = :orderId", OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
+    }
+}
